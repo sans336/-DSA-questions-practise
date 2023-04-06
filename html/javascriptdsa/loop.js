ae89f0a28a9465d1f98befe9ccd37df90b332abf
sans336 <sanskritij76@gmail.com>
2023-04-06 20:25:47 +0530

    first commit

---
+++ html/javascriptdsa/loop.js
@@ -0,0 +1,50 @@
+/* 1.Write a JS code to print numbers from 1 to 10
+
+for(i=1; i<=10; i++){
+    console.log(i);
+}
+
+/ 2.*0 100 200 300 400 500 600 700 800 900 1000
+
+for(i=0;i<1000;){
+    i+=100
+    console.log(i)
+}
+
+3). 1 2 4 8 16 32 64 128
+
+for(i=1;i<128;){
+    i=i*2
+    console.log(i)
+}
+4).0 2 4 6 8 10
+for (let i = 0; i <= 10; i=i+2) {
+    console.log(i);
+  }
+
+5.)  3 6 9 12 15
+  for (let i = 3; i <= 15; i=i+3) {
+    console.log(i);}
+
+6).9 8 7 6 5 4 3 2 1 0
+for(i=9;i>=0;i--){
+    console.log(i)
+}
+7).
+7.1 1 1 2 2 2 3 3 3 4 4 4
+*/
+for(i=1;i<=4;i++){
+    for(i=1;i<=4;i++){
+        console.log(i)
+    }
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
---
+++ javascript/days.js
@@ -0,0 +1,24 @@
+let sum = 10 + 20;
+console.log("Sum: ", sum); // 30
+
+const sum2 = 10 + 100;
+console.log("Sum: ", sum2);
+
+let num1 = 10;
+let num2 = 40;
+const finalSum = num1 + num2 + sum + sum2;
+console.log("Final Sum: ", finalSum);
+
+const resultV2 = "ankit" + 10;
+console.log("resultV2: ", resultV2, typeof resultV2);
+
+const resultV3 = "ankit" + "     " + "jain";
+console.log("resultV3: ", resultV3);
+
+const uName = "sanskriti";
+const uCity = "Denmark";
+const uCountry = "Europe";
+
+const greetingsMessage =
+"Welcome "+ uName + " to VSS India." + "  " + uName + " will be going to become perfect & skilled soon."
+console.log("greetingsMessage: ", greetingsMessage + ".");
